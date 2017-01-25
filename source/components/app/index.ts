149bef42a6edfc0f450a82ab69824c6a5ca95252
Cristian <isccristianr@gmail.com>
2017-01-25 17:16:22 -0600

    Agregando template y style para el componente app

--- source/components/app/index.ts
+++ source/components/app/index.ts
@@ -4,8 +4,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app',
-    template: `<h1>Hello {{name}}</h1>`
+	templateUrl: 'components/app/template.html',
+	styleUrls: [ 'components/app/style.css' ]
 })
 
 
-export class AppComponent { name = 'Angular'; }
+export class AppComponent {}
